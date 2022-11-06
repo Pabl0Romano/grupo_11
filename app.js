8b1baed046da7ff419e6ce734b8f34000c2b397e
Pabl0Romano <romae047@gmail.com>
2022-11-06 10:16:33 -0300

    footer

--- app.js
+++ app.js
@@ -15,12 +15,12 @@ app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"));
 });
 
-app.get("/",(req,res)=>{
+app.get("/producto",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/productDetail.html"));
 });
 
 
-app.get("/",(req,res)=>{
+app.get("/carrito",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/carrito.html"));
 });
 
