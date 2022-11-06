f51b825ea0a74a25c6f0c03e876f5ff042c70131
EliLuder <eliluder01@gmail.com>
2022-11-06 10:32:09 -0300

    Formulario y Login

--- app.js
+++ app.js
@@ -6,10 +6,17 @@ let PORT = process.env.PORT || 3000
 
 app.use(express.static("public"));
 
-app.listen(3000,()=>
+app.listen(3000,()=>{
     console.log("Servidor funcionando en puerto " + PORT)
-);
+});
 
 app.get("/",(req,res)=>{
     res.sendFile(path.join(__dirname,"/views/index.html"));
+});
+
+app.get("/register",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/register.html"));
+});
+app.get("/login",(req,res)=>{
+    res.sendFile(path.join(__dirname,"/views/login.html"));
 });
